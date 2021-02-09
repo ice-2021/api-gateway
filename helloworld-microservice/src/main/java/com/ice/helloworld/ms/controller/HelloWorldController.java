89d1e219a86da94489874e5bbbace22a204aba26
bnagaraju2 <bnagaraju2@sapient.com>
2021-02-09 13:00:49 +0530

    API Gateway Application

--- helloworld-microservice/src/main/java/com/ice/helloworld/ms/controller/HelloWorldController.java
+++ helloworld-microservice/src/main/java/com/ice/helloworld/ms/controller/HelloWorldController.java
@@ -22,5 +22,19 @@ public class HelloWorldController {
 	public ResponseEntity<String> showGreeting(@RequestBody HashMap<String, String> requestBody) {
 		return new ResponseEntity<String>("Welcome to ICE 2021 " + requestBody.get("name"), HttpStatus.OK);
 	}
-
+	
+	@GetMapping("/hi/1")
+	public ResponseEntity<String> hi(@RequestParam("name") String name ) {
+		return new ResponseEntity<String>(String.format("Hello Mr. %s Welcome to ICE 2021 " , name) , HttpStatus.OK);
+	}
+	
+	@GetMapping("/helloworld/hello")
+	public ResponseEntity<String> hello(@RequestParam("name") String name ) {
+		return new ResponseEntity<String>(String.format("Hello Mr. %s Welcome to ICE 2021 " , name) , HttpStatus.OK);
+	}
+	
+	@GetMapping("/service")
+	public ResponseEntity<String> service(@RequestParam("name") String name ) {
+		return new ResponseEntity<String>(String.format("Hello Mr. %s Welcome to ICE 2021 " , name) , HttpStatus.OK);
+	}
 }
